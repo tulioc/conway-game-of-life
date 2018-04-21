4e8983ba10d309db099ba9bd7e7d4ecb8b5585ed
Tulio Coelho <tulioopc@gmail.com>
2018-04-21 02:37:10 -0300

    Refactoring check neighbours around method

--- world.rb
+++ world.rb
@@ -1,11 +1,16 @@
 require_relative 'cell.rb'
 
 class World
-  attr_accessor :rows, :cols, :cell_grid, :cells
+  attr_accessor :rows, :cols, :cell_grid, :cells, :first_row, :first_column,
+  :last_row, :last_column
 
   def initialize(rows=3, cols=3)
+    @first_row = 0
+    @first_column = 0
     @rows = rows
     @cols = cols
+    @last_row = (@rows - 1)
+    @last_column = (@cols -1)
     @cells = []
     @cell_grid = Array.new(rows) do |row|
       Array.new(cols) do |col|
@@ -16,58 +21,74 @@ class World
     end
   end
 
+  def check_north_neighbour(live_neighbours, cell)
+    if cell.current_row > @first_row
+      neighbour = self.cell_grid[cell.row_above][cell.current_column]
+      live_neighbours << neighbour if neighbour.alive?
+    end
+  end
+
+  def check_north_east_neighbour(live_neighbours, cell)
+    if cell.current_row > @first_row && cell.current_column < @last_column
+      neighbour = self.cell_grid[cell.row_above][cell.right_column]
+      live_neighbours << neighbour if neighbour.alive?
+    end
+  end
+
+  def check_east_neighbour(live_neighbours, cell)
+    if cell.current_column < @last_column
+      neighbour = self.cell_grid[cell.current_row][cell.right_column]
+      live_neighbours << neighbour if neighbour.alive?
+    end
+  end
+
+  def check_south_east_neighbour(live_neighbours, cell)
+    if cell.current_row < @last_row && cell.current_column < @last_column
+      neighbour = self.cell_grid[cell.row_below][cell.right_column]
+      live_neighbours << neighbour if neighbour.alive?
+    end
+  end
+
+  def check_south_neighbour(live_neighbours, cell)
+    if cell.current_row < @last_row
+      neighbour = self.cell_grid[cell.row_below][cell.current_column]
+      live_neighbours << neighbour if neighbour.alive?
+    end
+  end
+
+  def check_south_west_neighbour(live_neighbours, cell)
+    if cell.current_row < @last_row && cell.current_column > @first_column
+      neighbour = self.cell_grid[cell.row_below][cell.left_column]
+      live_neighbours << neighbour if neighbour.alive?
+    end
+  end
+
+  def check_west_neighbour(live_neighbours, cell)
+    if cell.current_column > @first_column
+      neighbour = self.cell_grid[cell.current_row][cell.left_column]
+      live_neighbours << neighbour if neighbour.alive?
+    end
+  end
+
+  def check_north_west_neighbour(live_neighbours, cell)
+    if cell.current_row > @first_row && cell.current_column > @first_column
+      neighbour = self.cell_grid[cell.row_above][cell.left_column]
+      live_neighbours << neighbour if neighbour.alive?
+    end
+  end
+
+
   def live_neighbours_around_cell(cell)
     live_neighbours = []
-    first_row = 0
-    first_column = 0
-    actual_row = (cell.row_y)
-    actual_column = (cell.column_x)
-    row_above = (actual_row - 1)
-    row_below = (actual_row + 1)
-    right_column = (actual_column + 1)
-    left_column = (actual_column - 1)
-    last_column = (cols - 1)
-    last_row = (rows - 1)
 
-    if actual_row > first_row
-      candidate = self.cell_grid[row_above][actual_column]
-      live_neighbours << candidate if candidate.alive?
-    end
-
-    if actual_row > first_row && actual_column < last_column
-      candidate = self.cell_grid[row_above][right_column]
-      live_neighbours << candidate if candidate.alive?
-    end
-
-    if actual_column < last_column
-      candidate = self.cell_grid[actual_row][right_column]
-      live_neighbours << candidate if candidate.alive?
-    end
-
-    if actual_row < last_row && actual_column < last_column
-      candidate = self.cell_grid[row_below][right_column]
-      live_neighbours << candidate if candidate.alive?
-    end
-
-    if actual_row < last_row
-      candidate = self.cell_grid[row_below][actual_column]
-      live_neighbours << candidate if candidate.alive?
-    end
-
-    if actual_row < last_row && actual_column > first_column
-      candidate = self.cell_grid[row_below][left_column]
-      live_neighbours << candidate if candidate.alive?
-    end
-
-    if actual_column > first_column
-      candidate = self.cell_grid[actual_row][left_column]
-      live_neighbours << candidate if candidate.alive?
-    end
-
-    if actual_row > first_row && actual_column > first_column
-      candidate = self.cell_grid[row_above][left_column]
-      live_neighbours << candidate if candidate.alive?
-    end
+    check_north_neighbour(live_neighbours, cell)
+    check_north_east_neighbour(live_neighbours, cell)
+    check_east_neighbour(live_neighbours, cell)
+    check_south_east_neighbour(live_neighbours, cell)
+    check_south_neighbour(live_neighbours, cell)
+    check_south_west_neighbour(live_neighbours, cell)
+    check_west_neighbour(live_neighbours, cell)
+    check_north_west_neighbour(live_neighbours, cell)
 
     return live_neighbours
   end
